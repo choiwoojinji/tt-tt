2dbb8e8196c906afd1423e006569cb2166d0cf88
choiwoojinji <cbywjc123@gmail.com>
2026-08-09 20:37:13 +0900

    익혀보는 연습

--- practice.py
+++ practice.py
@@ -5,3 +5,12 @@
 # git push 순서예요
 
 # 처음부터 큰틀이라도 익혀봅시다
+import pathlib
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+texts = {
+    p.stem: p.read_test(encoding='utf-8') # 인코딩은 utf-8 로 함
+    for p in sorted(pathlib.Path("posts").glob("*.md")) 
+    # 원본보존하고 복사로 정렬함 posts 파일안에 md파일 전부 가지고오겠다
+}
+
